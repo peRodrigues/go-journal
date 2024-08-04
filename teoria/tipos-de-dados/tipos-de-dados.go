78686354818d245303d34fc4718cafacaabda7b0
Pedro Rodrigues <p.rodrigues1229@gmail.com>
2024-08-03 23:28:15 -0300

    Adicionei comentários explicativos ao arquivo tipos-de-dados.go

--- teoria/tipos-de-dados/tipos-de-dados.go
+++ teoria/tipos-de-dados/tipos-de-dados.go
@@ -4,10 +4,12 @@ import "fmt"
 
 func Tiposdedados() {
 	fmt.Println("Tipos de dados:")
+	// Declaração completa
 	var inteiro int = 13
 	var mininteiro int8 = 127
 	fmt.Printf("Valor: %v\tTipo: %T\nValor: %v\tTipo: %T\n", inteiro, inteiro, mininteiro, mininteiro)
 
+	// Declaração da variável com tipo inferido como string
 	texto := `"Hello, World"`
 	fmt.Printf("Valor: %v\tTipo: %T\n", texto, texto)
 }
